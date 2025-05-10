2334c7a40a3172843169d8d55ebd9602af23a363
QassimMohamed <Qmohr2005@gmail.com>
2025-05-10 14:24:16 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+// Name1: Qassim Mohammed Ahmed 	Student ID1: 202203800
+// Name2: Ali Othman Abbas 			Student ID2: 202208581
+// Name2: RABEE ABDALKARIM 		    Student ID3: 202206837
 public class Main {
     public static void main(String[] args) {
         Interface interfaceObj = new Interface();
